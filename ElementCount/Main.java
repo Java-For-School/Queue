76013522824d04ebc373c76a4896dd0f793f6e44
SorerBOT <alonfiller@gmail.com>
2023-04-04 11:56:04 +0300

    (Feat) element count without new queue

--- ElementCount/Main.java
+++ ElementCount/Main.java
@@ -1,4 +1,5 @@
 import java.util.Queue;
+import java.lang.ProcessBuilder.Redirect.Type;
 import java.util.LinkedList;
 
 public class Main {
@@ -9,7 +10,7 @@ public class Main {
         queue.add(2);
         queue.add(3);
 
-        System.out.printf("The amount of elements in the queue is: %d\n", ElementCount(queue));
+        System.out.printf("The amount of elements in the queue is: %d\n", ElementCountWithoutNewQueue(queue));
     }
     private static int ElementCount(Queue<Integer> queue) {
         Queue<Integer> temp = new LinkedList<Integer>();
@@ -22,6 +23,17 @@ public class Main {
 
         while (!temp.isEmpty()) queue.add(temp.poll());
 
+        return count;
+    }
+    private static int ElementCountWithoutNewQueue(Queue<Integer> queue) {
+        int count = 0;
+
+        queue.add(Integer.MAX_VALUE);
+
+        while (queue.peek() != Integer.MAX_VALUE) {
+            queue.add(queue.poll());
+            ++count;
+        }
         return count;
     }
 }
